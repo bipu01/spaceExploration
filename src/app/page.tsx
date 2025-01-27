7e489266850cc0e1b607f6ace98468acc4cff918
bipu01 <biplovegautam123@gmail.com>
2025-01-27 15:54:50 +0545

    build problem fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import Scene from "@/components/Scene";
-import React, { Suspense, useState } from "react";
-import { motion } from "framer-motion";
-// import EarthNew from "../../public/earth/EarthNew";
-import Saturn from "@/components/Saturn";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Image from "next/image";
-import bg from "../../public/images/bg.jpeg";
+import React from "react";
 
-import MainScene from "@/components/MainScene";
+// import MainScene from "@/components/MainScene";
 import LandingPage from "./PageSegments/LandingPage";
 import Navbar from "./PageSegments/Navbar";
 import PlanetsPreview from "./PageSegments/PlanetsPreview";
-
-import SinglePlanetView from "./components/SinglePlanetView";
 
 export default function Page() {
     return (
